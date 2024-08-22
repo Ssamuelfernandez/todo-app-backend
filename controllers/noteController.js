22ec0be851157c00600d65419f9d003998c36dfc
Samuel <rpbertofernandezsepulveda@gmail.com>
2024-08-22 18:49:22 +0200

    Add notes routes

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -7,4 +7,21 @@ export class NoteController {
         const notes = await NoteModel.getNotes();
             res.json(notes);
     }
+
+    static async getNotesById(req, res) {
+
+    }
+
+    static async postNotes(req, res) {
+
+    }
+
+    static async patchNotes(req, res) {
+
+    }
+
+    static async deleteNotes(req, res) {
+
+    }
+    
 }
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -4,3 +4,7 @@ import { NoteController } from '../controllers/noteController.js'
 export const noteRouter = Router()
 
 noteRouter.get("/notes",NoteController.getNotes)
+noteRouter.get("/notes/:id", NoteController.getNotesById)
+noteRouter.post("/notes", NoteController.postNotes)
+noteRouter.patch("/notes/:id", NoteController.patchNotes)
+noteRouter.delete("/notes/:id", NoteController.deleteNotes)
